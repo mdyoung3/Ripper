fe8ce551796ca30f27f4bc60ad5428cf5aef12a4
Marc D. Young <marcdeeyoung@gmail.com>
2019-10-03 21:43:29 -0600

    Updated vobber function to allow more interaction. Added YAML files as variables to Ripper. Started refactoring Ripper.

--- Ripper-v2.sh
+++ Ripper-v2.sh
@@ -3,16 +3,43 @@
 # Copyright (c) 2019 Marc Young
 #
 # encode for playback on Roku or Chromcast
+# 
+# shellcheck disable=SC1091
+# shellcheck disable=SC2154
 
 set -e
 
 # shellcheck source=includes/helpers
 source includes/helpers  
+source includes/yaml.sh 
 
-cd "$1" || exit
+create_variables properties.yml
+
+cd ~/Videos
+
+makedir "$title"
+
+echo "Starting the $title"
+
+echo "$episode" > episode
+
+echo "$season" > season
+
+newseason=$(cat episode)
+
+echo "$newseason"
+
+exit
 
 for d in *; do
  cd "$d" || exit
+ rip 
 done
+
+rm episode
+
+echo "................."
+
+echo "Script is complete."
 
 exit
